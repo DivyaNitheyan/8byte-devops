714947f9e3d194997daf219e9a394f678c54e89e
Divya Nitheyan Sundaram <dsundaram@aptean.com>
2026-05-19 02:25:18 +0530

    Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -158,11 +158,11 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-  from_port   = 3100
-  to_port     = 3100
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+    from_port   = 3100
+    to_port     = 3100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
